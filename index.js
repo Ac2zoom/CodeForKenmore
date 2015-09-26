31dd5582d220ebf79762a36216d669c99d341fa1
Ac2zoom <ac2zoom@yahoo.com>
2015-09-26 15:47:22 -0700

    Initial Functionality

---
+++ index.js
@@ -0,0 +1,37 @@
+/**
+ * Created by akshay on 9/26/15.
+ */
+
+var map = L.map('map').setView([47.76, -122.25], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+    'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(map);
+
+var appToken = 'KIfXfEDIcraE5i0dxIb6YkzHN';
+
+var query = "'MEDICAL OFFICE (341)'";
+var query2 = "'MULTIPLE RESIDENCES ASSISTED LIVING (LOW RISE)'";
+
+function a(inputQuery){
+    $.getJSON("https://kenmorewa.data.socrata.com/resource/b6gg-b3a4.json?$$app_token=" + appToken + "&$where=predominant_use=" + inputQuery +  " AND zip_code='98028'",function(data) {
+        for(var i = 0; i < data.length; i++){
+            L.marker([data[i].latitude, data[i].longitude]).addTo(map);
+        }
+    });
+}
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
